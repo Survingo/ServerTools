7094f11a416a50322d09964b8a3f85d50c124e3b
Survingo <thehydra.tk@gmail.com>
2016-05-09 17:24:00 +0200

    new features

--- Survingo/ServerTools/ServerTools.php
+++ Survingo/ServerTools/ServerTools.php
@@ -26,13 +26,23 @@ use pocketmine\plugin\Plugin;
 use pocketmine\Server;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\utils\Utils;
+use pocketmine\Player;
 
 class ServerTools extends PluginBase implements Listener{
 
     public function onEnable(){
-
-                  $this->getLogger()->info(TF::GREEN . "ServerTools v" . $this->getDescription()->getVersion() . " by Survingo enabled!");
+    
+                  $this->getLogger()->info(TF::GREEN . "Enabling ServerTools v" . $this->getDescription()->getVersion() . " by Survingo...");
+                  $this->saveDefaultConfig();
+                  $this->getLogger()->info(TF::GREEN . "Checking configuration...");
+                  $this->checkConfiguration(); // Who knows what people may write there
                   
+                  if($this->getConfig()->getNested("autoupdater.autoupdater") === true){
+                    $this->checkInternetConnection();
+                    }else{
+                      $this->getLogger()->warning("AutoUpdater disabled!");
+                      }   
 
     }
 
@@ -56,22 +66,24 @@ class ServerTools extends PluginBase implements Listener{
                     }
                     $arg = array_shift($args);
                     switch ($arg) {
-                        
+                                          
                case "help":
                case "?":
                             if($sender->hasPermission("servertools.command.help")){
-                            $sender->sendMessage(" ");
-                            $sender->sendMessage("[§6Server§eTools§f] --- Showing help page 1 of 1 (/servertools help§7/§f? <page>) ---");
-                            $sender->sendMessage("§2/tools help§f/§2?: §fShows the help");
-                            $sender->sendMessage("§2/tools enable <PluginName>: §fEnables a disabled Plugin");
-                            $sender->sendMessage("§2/tools disable <PluginName>: §fDisables a enabled Plugin");
-                            $sender->sendMessage("§2/tools restart <PluginName>: §fRestarts a enabled Plugin");
-                            $sender->sendMessage(" ");
-		
-                                return true;
-                            } else {
-                                $sender->sendMessage("§cYou don't have the permission to do that!");
-                            }
+                              if(count($args) === 0 or trim(implode(" ", $args)) === "1"){
+                                $sender->sendMessage("[§6Server§eTools§f] --- Showing help page 1 of 2 (/servertools help <page>) ---");
+                                $sender->sendMessage("§2/tools help: §fShows the help");
+                                $sender->sendMessage("§2/tools ?: §7Alias for /tools help");
+                                $sender->sendMessage("§2/tools enable <PluginName>: §fEnables a disabled Plugin");
+                                $sender->sendMessage("§2/tools disable <PluginName>: §fDisables a enabled Plugin");
+                                $sender->sendMessage("§2/tools restart <PluginName>: §fRestarts a enabled Plugin");
+                                   
+                                }elseif(trim(implode(" ", $args)) === "2"){  
+                                $sender->sendMessage("§2/tools update: §fChecks for an Update");
+                                $sender->sendMessage("§2/tools check: §7Alias for /tools update");
+                                $sender->sendMessage("§e/tools config: §fChecks the config.yml §l§6BETA");
+                                }
+                              }  
                             return true;
                             break;
                 case "disable":
@@ -165,10 +177,72 @@ class ServerTools extends PluginBase implements Listener{
           }
         }
         break;
+
+                case "update":
+                case "check":
+                if($sender->hasPermission("servertools.command.update")){
+		if(count($args) === 0){
+		  if(!$sender instanceof Player){
+			$sender->sendMessage("[§6Server§eTools§f] Checking for an Update...");
+			$this->checkInternetConnection();
+			return true;
+			   }else{
+			     $sender->sendMessage("§cYou can't check for an Update In-Game!");
+			     return true;
+			     }
+          }
+        }
+        break;
                 
       }
     }
   }
   
+    public function checkConfiguration(){
+    
+                   $config = $this->getConfig();
+                       
+                   if($config->get("version") !== $this->getDescription()->getVersion()){
+                     $this->getLogger()->error("The current configuration was made for §6" . $config->get("version") . "§4, but you have installed version §6" . $this->getDescription()->getVersion() . "§4. Please delete the current '§6config.yml§4' and restart your server.");
+                     $this->getServer()->getPluginManager()->disablePlugin($this->getServer()->getPluginManager()->getPlugin($this->getDescription()->getName()));
+                   }elseif(!(($config->getNested("autoupdater.channel") == "stable") or ($config->getNested("autoupdater.channel") == "beta"))){
+                     $this->getLogger()->error("Your preffered channel in the configuration '§c" . $config->getNested("autoupdater.channel") . "§4' is not available! Please choose between '§6stable§4' or '§6beta§4' and restart your Server.");
+                     $this->getServer()->getPluginManager()->disablePlugin($this->getServer()->getPluginManager()->getPlugin($this->getDescription()->getName()));
+                   }else{
+                     $this->getLogger()->info(TF::GREEN . "Configuration is valid!");
+                     }
+
+  }  
+  
+    public function checkInternetConnection(){
+    
+                   $this->getLogger()->info(TF::GREEN . "Checking Internet Connection...");
+                   //if(Utils::getURL("https://raw.githubusercontent.com/Survingo/AutoUpdater/master/InternetTest.txt") == "true"){
+                     $this->getLogger()->info(TF::GREEN . "You are connected to the Internet! Now checking for an Update...");
+                     $this->checkForUpdate();
+                     //}else{
+                      //$this->getLogger()->error("Can't check for an Update, because you are not connected to the Internet!");
+                      //}
+
+  }
+  
+    public function checkForUpdate(){
+                  
+                      $versionURL = "https://raw.githubusercontent.com/Survingo/AutoUpdater/master/ServerTools/version.txt";
+                      $VersionTXT = Utils::getURL($versionURL);
+                      $parsedVersion = yaml_parse($VersionTXT);
+                    
+                      $channelURL = "https://raw.githubusercontent.com/Survingo/AutoUpdater/master/ServerTools/channel.txt";
+                      $channelTXT = Utils::getURL($channelURL);
+                      $parsedChannel = yaml_parse($channelTXT);
+                    
+                      if($parsedVersion !== $this->getDescription()->getVersion() and $parsedChannel == $this->getConfig()->getNested("autoupdater.channel")){
+                        $this->getLogger()->info("\n------------------------\n§l§6A new " . $this->getConfig()->getNested("autoupdater.channel") . " version of ServerTools is available!§r\n§eYour version:§6\n" . $this->getDescription()->getVersion() . "\n§eNew version:§6\n" . Utils::getURL("https://raw.githubusercontent.com/Survingo/AutoUpdater/master/ServerTools/version.txt") . "\n§eUpdate Info:§6 " . Utils::getURL("https://raw.githubusercontent.com/Survingo/AutoUpdater/master/ServerTools/info.txt") . "\n§eUpdate now at §a" . Utils::getURL("https://raw.githubusercontent.com/Survingo/AutoUpdater/master/ServerTools/download.txt") . "\n\n§f------------------------");
+                        }elseif($parsedVersion !== $this->getDescription()->getVersion() and $parsedChannel !== $this->getConfig()->getNested("autoupdater.channel")){
+                          $this->getLogger()->info("\n------------------------\n§l§6A new " . $this->getConfig()->getNested("autoupdater.channel") . " version of ServerTools is available!§r\n§eYour version:§6\n" . $this->getDescription()->getVersion() . "\n§eNew version:§6\n" . Utils::getURL("https://raw.githubusercontent.com/Survingo/AutoUpdater/master/ServerTools/version.txt") . "\n§eUpdate Info:§6 " . Utils::getURL("https://raw.githubusercontent.com/Survingo/AutoUpdater/master/ServerTools/info.txt") . "\n§eUpdate now at §a" . Utils::getURL("https://raw.githubusercontent.com/Survingo/AutoUpdater/master/ServerTools/download.txt") . "\n\n§f------------------------");
+                          }else{
+                            $this->getLogger()->info("§aYour version is up to date!");
+                           }
+  }
   
 }
